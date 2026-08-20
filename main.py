703be259c2bbcdc839ce25cbbff51c61463c6355
Sean Coleman <bramblestar1.0@icloud.com>
2026-08-19 23:52:21 -0700

    grab env from -revious project

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import gymnasium as gym
 import gym_carla
 import carla
+import os
+import warnings
+warnings.filterwarnings("ignore")
+from torch import multiprocessing
+
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import torch
+from tensordict.nn import TensorDictModule
+from tensordict.nn.distributions import NormalParamExtractor
+from torch import multiprocessing, nn
+
+from torchrl.collectors import Collector
+from torchrl.data.replay_buffers import ReplayBuffer
+from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+from torchrl.data.replay_buffers.storages import LazyTensorStorage
+from torchrl.envs import (
+    Compose,
+    CatFrames,
+    ToTensorImage,
+    StepCounter,
+    TransformedEnv,
+)
+from torchrl.envs.libs.gym import GymEnv
+from torchrl.envs.utils import check_env_specs, ExplorationType, set_exploration_type
+from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
+from torchrl.objectives import ClipPPOLoss
+from torchrl.objectives.value import GAE
+from tqdm import tqdm
+
 
 # parameters for the gym_carla environment
 params = {
@@ -28,5 +59,28 @@ params = {
 'display_route': False,  # whether to render the desired route
 }
 
-# Set gym-carla environment
-env = gym.make('carla-v0', params=params)
+is_fork = multiprocessing.get_start_method() == "fork"
+device = (
+    torch.device(0)
+    if torch.cuda.is_available() and not is_fork
+    else torch.device("cpu")
+)
+torch.set_default_device(device)
+num_cells = 256  # number of cells in each layer i.e. output dim.
+lr = 8e-5
+max_grad_norm = 1.0
+
+frames_per_batch = 1000
+# For a complete training, bring the number of frames up to 1M
+total_frames = 750000
+
+sub_batch_size = 64  # cardinality of the sub-samples gathered from the current data in the inner loop
+num_epochs = 10  # optimization steps per batch of data collected
+clip_epsilon = (
+    0.2  # clip value for PPO loss: see the equation in the intro for more context.
+)
+gamma = 0.99
+lmbda = 0.95
+entropy_eps = 12e-3
+
+base_env = GymEnv("carla-v0", params=params, render_mode="human", lap_complete_percent=0.95, domain_randomize=False, continuous=True, device=device)
