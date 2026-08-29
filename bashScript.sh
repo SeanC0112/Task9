7288c8f89b7c1de596596367e94b11038b84af67
Sean Coleman <bramblestar1.0@icloud.com>
2026-08-29 03:07:01 -0700

    import old bashscript

---
+++ bashScript.sh
@@ -0,0 +1,11 @@
+apt install python3.10 python3.10-venv python3.10-dev -y
+apt install pip -y
+# apt-get install -y swig
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python3 main.py baseline
